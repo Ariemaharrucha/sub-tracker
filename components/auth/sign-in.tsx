5c41b986096c24a57b65aae6e9e6b4c5c49122bf
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2025-12-02 21:29:11 +0700

    refactor: improve sign-in button layout and styling

--- components/auth/sign-in.tsx
+++ components/auth/sign-in.tsx
@@ -5,17 +5,16 @@ import { Button } from "../ui/button";
 
 export default function SignIn() {
   return (
-    <div className="flex justify-center items-center h-screen relative">
-      <div className="p-4 absolute top-4 right-4">
-      </div>
+    <div className="flex justify-center items-center h-screen">
       <div className="w-full max-w-md px-4 ">
-        <h1 className="mb-6 text-center text-2xl font-bold text-slate-900">
+        <h1 className="mb-6 text-center text-2xl font-semibold text-slate-900">
           Sign In
         </h1>
         <Button
           type="button"
           variant="outline"
-          className="w-full cursor-pointer"
+          className="w-full cursor-pointer border-slate-500 rounded-full"
+          size={"lg"}
           onClick={async () => {
             await signIn.social({
               provider: "google",
@@ -23,7 +22,14 @@ export default function SignIn() {
             });
           }}
         >
-          Continue with Google
+          <img
+            src="https://cdn.shadcnstudio.com/ss-assets/brand-logo/google-icon.png?width=20&height=20&format=auto"
+            alt="Google Icon"
+            className="size-5"
+          />
+          <span className="flex flex-1 justify-center">
+            Continue with Google
+          </span>
         </Button>
       </div>
     </div>
