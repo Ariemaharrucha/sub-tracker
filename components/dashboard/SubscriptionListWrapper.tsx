2754f7e587562b5c23a9c17c7890431f22d68cd8
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2025-12-07 21:02:43 +0700

    update table

--- components/dashboard/SubscriptionListWrapper.tsx
+++ components/dashboard/SubscriptionListWrapper.tsx
@@ -15,28 +15,29 @@ export default function SubscriptionListWrapper({ userId, subscriptions }: { use
   );
 
   return (
-    <>
-    <div className="mt-6 flex justify-between items-center gap-8">
-      <div className="w-full">
-        <div className="relative">
-          <div className="text-muted-foreground pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-3 peer-disabled:opacity-50">
-            <SearchIcon className="size-4 text-amber-600" />
-            <span className="sr-only">Search</span>
+    <div className="mt-6 bg-gradient-to-br from-orange-50 via-amber-50 to-rose-50 p-6 rounded-xl shadow-md">
+      <h3 className="font-semibold text-2xl ml-2">Subscription List</h3>
+      <div className="mt-6 flex justify-between items-center gap-8">
+        <div className="w-full">
+          <div className="relative">
+            <div className="text-muted-foreground pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-3 peer-disabled:opacity-50">
+              <SearchIcon className="size-4 text-amber-600" />
+              <span className="sr-only">Search</span>
+            </div>
+            <Input
+              type="search"
+              placeholder="Search subscription..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              className="px-9 h-10 rounded-lg border-amber-600 bg-white text-amber-950 placeholder:text-amber-600 focus:border-amber-400 focus:bg-white transition-colors focus:ring-2 focus:ring-amber-400 focus:ring-offset-2 focus:ring-offset-amber-400 focus-visible:ring-2 focus-visible:ring-amber-400 focus-visible:ring-offset-2 focus-visible:ring-offset-amber-400 focus-visible:outline-none"
+            />
           </div>
-          <Input
-            type="search"
-            placeholder="Search subscription..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className="px-9 h-10 rounded-lg border-amber-200 bg-amber-50 text-amber-950 placeholder:text-amber-600 focus:border-amber-400 focus:bg-white transition-colors focus:ring-2 focus:ring-amber-400 focus:ring-offset-2 focus:ring-offset-amber-400 focus-visible:ring-2 focus-visible:ring-amber-400 focus-visible:ring-offset-2 focus-visible:ring-offset-amber-400 focus-visible:outline-none"
-          />
+        </div>
+        <div>
+          <AddSubscriptionDialog userId={userId} />
         </div>
       </div>
-      <div>
-        <AddSubscriptionDialog userId={userId} />
-      </div>
+      <ListSubscriptionClient subscriptions={filtered} />
     </div>
-    <ListSubscriptionClient subscriptions={filtered} />
-    </>
   );
 }
